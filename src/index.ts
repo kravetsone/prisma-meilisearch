5f874f463a5110d4ff2f86767ab029893468a9fb
Kravets <57632712+kravetsone@users.noreply.github.com>
2023-09-02 23:30:22 +0300

    feat: add new sync methods, types and improves

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Prisma as RuntimePrisma } from "@prisma/client";
 import { Prisma } from "@prisma/client/extension";
 import { MeiliSearch } from "meilisearch";
+import { SearchResponse } from "meilisearch/dist/types/types";
 
 type TModelNames = keyof typeof RuntimePrisma.ModelName;
 type TExtension = ReturnType<
@@ -8,65 +9,79 @@ type TExtension = ReturnType<
         NonNullable<unknown>,
         {
             $allModels: {
-                search: (query: string) => Promise<number[]>;
+                search: (query: string) => Promise<SearchResponse>;
             };
         }
     >
 >;
 type TModelTransform = (model: any) => Record<string, unknown>;
 
-const syncOnOperations = ["create", "update", "upsert", "delete"];
-
 export class PrismaMeilisearch {
-    private readonly client: MeiliSearch;
     extension: TExtension;
-    models: Record<TModelNames, TModelTransform | true>;
 
     constructor(
         client: MeiliSearch,
         params: { models: Record<TModelNames, TModelTransform | true> },
     ) {
-        this.client = client;
-        this.models = params.models;
+        this.extension = Prisma.defineExtension({
+            name: "prisma-meilisearch",
+            model: {
+                $allModels: {
+                    async search(query: string) {
+                        const modelName = RuntimePrisma.getExtensionContext(
+                            this,
+                        ).$name as TModelNames;
 
-        this.extension = Prisma.defineExtension((prisma) =>
-            prisma.$extends({
-                name: "prisma-meilisearch",
-                model: {
-                    $allModels: {
-                        async search(query: string) {
-                            const modelName = RuntimePrisma.getExtensionContext(
-                                this,
-                            ).$name as TModelNames;
+                        if (!Object.keys(params.models).includes(modelName))
+                            throw new Error(
+                                `The «${modelName}» model does not exist in PrismaMeilisearch`,
+                            );
 
-                            if (!Object.keys(params.models).includes(modelName))
-                                throw new Error(
-                                    `The «${modelName}» model does not exist in PrismaMeilisearch`,
-                                );
-
-                            return client.index(modelName).search(query);
-                        },
+                        return client.index(modelName).search(query);
                     },
                 },
-                query: {
-                    $allModels: {
-                        async create({ model, operation, args, query }) {
-                            const data = await query(args);
-                            const hook = params.models[model as TModelNames];
+            },
+            query: {
+                $allModels: {
+                    async create({ model, args, query }) {
+                        const data = await query(args);
+                        const hook = params.models[model as TModelNames];
 
-                            await client.index(model).addDocuments([
-                                hook === true
-                                    ? {
-                                          ...(data as Record<string, unknown>),
-                                      }
-                                    : (hook as TModelTransform)(data),
-                            ]);
+                        await client.index(model).addDocuments([
+                            hook === true
+                                ? {
+                                      ...(data as Record<string, unknown>),
+                                  }
+                                : (hook as TModelTransform)(data),
+                        ]);
 
-                            return data;
-                        },
+                        return data;
+                    },
+                    async update({ model, args, query }) {
+                        const data = await query(args);
+                        const hook = params.models[model as TModelNames];
+
+                        await client.index(model).updateDocuments([
+                            hook === true
+                                ? {
+                                      ...(data as Record<string, unknown>),
+                                  }
+                                : (hook as TModelTransform)(data),
+                        ]);
+
+                        return data;
+                    },
+                    async delete({ model, args, query }) {
+                        const data = await query(args);
+
+                        //TODO: add select primaryKey support
+                        // @ts-ignore
+                        await client.index(model).deleteDocument(data!.id);
+
+                        return data;
                     },
                 },
-            }),
-        );
+            },
+        });
     }
 }
